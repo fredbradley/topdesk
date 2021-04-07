16fc4c4b45150c50f3e9fbb584eb7079a160b284
Fred Bradley <code@fredbradley.co.uk>
2021-04-07 16:54:45 +0100

    fix: keep the cache forever, to prevent double access (this can be resolve in Laravel 8 with Cache Locking)

--- src/Commands/GetTopDeskCounts.php
+++ src/Commands/GetTopDeskCounts.php
@@ -2,7 +2,6 @@
 
 namespace FredBradley\TOPDesk\Commands;
 
-use FredBradley\Cacher\EasySeconds;
 use FredBradley\TOPDesk\Facades\TOPDesk;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
@@ -40,37 +39,37 @@ class GetTopDeskCounts extends Command
      */
     public function handle()
     {
-        $cache = Cache::remember('topdeskApiCounts',
-            EasySeconds::minutes(5), function () {
-                $userClosedCounts = collect(TOPDesk::resolveCountsForOperatorGroup(
-                    'I.T. Services',
-                    ['HELPDESK']
-                ))->sortByDesc('closed_week');
+        Cache::forget('topdeskApiCounts');
+        Cache::rememberForever('topdeskApiCounts', function () {
+            $userClosedCounts = collect(TOPDesk::resolveCountsForOperatorGroup(
+                'I.T. Services',
+                ['HELPDESK']
+            ))->sortByDesc('closed_week');
 
-                return response()->json([
-                    'unresolvedIncidents' => TOPDesk::countOpenTickets(),
-                    'openChangeActivities' => count(TOPDesk::allOpenChangeActivities()),
-                    'loggedToday' => TOPDesk::countTicketsLoggedToday(), // + TOPDesk::countChangesLoggedToday(),
-                    'unassigned' => TOPDesk::countUnassignedTickets() + count(TOPDesk::unassignedWaitingChangeActivities()),
-                    'inProgress' => TOPDesk::countTicketsByStatus('In progress'),
-                    'waitingForUser' => TOPDesk::countTicketsByStatus('Waiting for user'),
-                    'updatedByUser' => TOPDesk::countTicketsByStatus('Updated by user'),
-                    'waitingForSupplier' => TOPDesk::countTicketsByStatus('Waiting for supplier'),
-                    'scheduled' => TOPDesk::countTicketsByStatus('Scheduled'),
-                    'dueThisWeek' => TOPDesk::countTicketsDueThisWeek(),
-                    'breachedTickets' => TOPDesk::countBreachedTickets(),
-                    'usersClosedCounts' => $userClosedCounts,
-                    'collectiveClosesByDay' => $userClosedCounts->map(function ($item) {
-                        return $item['closed_day'];
-                    })->sum(),
-                    'collectiveClosesByWeek' => $userClosedCounts->map(function ($item) {
-                        return $item['closed_week'];
-                    })->sum(),
-                    'collectiveClosesByTotal' => $userClosedCounts->map(function ($item) {
-                        return $item['closed_total'];
-                    })->sum(),
-                ]);
-            });
+            return response()->json([
+                'unresolvedIncidents' => TOPDesk::countOpenTickets(),
+                'openChangeActivities' => count(TOPDesk::allOpenChangeActivities()),
+                'loggedToday' => TOPDesk::countTicketsLoggedToday(), // + TOPDesk::countChangesLoggedToday(),
+                'unassigned' => TOPDesk::countUnassignedTickets() + count(TOPDesk::unassignedWaitingChangeActivities()),
+                'inProgress' => TOPDesk::countTicketsByStatus('In progress'),
+                'waitingForUser' => TOPDesk::countTicketsByStatus('Waiting for user'),
+                'updatedByUser' => TOPDesk::countTicketsByStatus('Updated by user'),
+                'waitingForSupplier' => TOPDesk::countTicketsByStatus('Waiting for supplier'),
+                'scheduled' => TOPDesk::countTicketsByStatus('Scheduled'),
+                'dueThisWeek' => TOPDesk::countTicketsDueThisWeek(),
+                'breachedTickets' => TOPDesk::countBreachedTickets(),
+                'usersClosedCounts' => $userClosedCounts,
+                'collectiveClosesByDay' => $userClosedCounts->map(function ($item) {
+                    return $item[ 'closed_day' ];
+                })->sum(),
+                'collectiveClosesByWeek' => $userClosedCounts->map(function ($item) {
+                    return $item[ 'closed_week' ];
+                })->sum(),
+                'collectiveClosesByTotal' => $userClosedCounts->map(function ($item) {
+                    return $item[ 'closed_total' ];
+                })->sum(),
+            ]);
+        });
 
         return 0;
     }
