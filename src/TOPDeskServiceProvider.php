5c3b03c3b9db3391fdd8cf8245f4bd38bae1183f
Fred Bradley <code@fredbradley.co.uk>
2021-04-07 16:29:26 +0100

    feat: remove routes file from package

--- src/TOPDeskServiceProvider.php
+++ src/TOPDeskServiceProvider.php
@@ -17,7 +17,7 @@ class TOPDeskServiceProvider extends ServiceProvider
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'fredbradley');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'fredbradley');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
